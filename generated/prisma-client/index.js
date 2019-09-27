5775b6b9f19e485357140486590ffc4c769575b8
Noh Hyeon Jin <hjforj@naver.com>
2019-09-27 16:34:21 +0900

    Create index.js

---
+++ generated/prisma-client/index.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Category",
+    embedded: false
+  },
+  {
+    name: "Store",
+    embedded: false
+  },
+  {
+    name: "Menu",
+    embedded: false
+  },
+  {
+    name: "ChatRoom",
+    embedded: false
+  },
+  {
+    name: "Order",
+    embedded: false
+  },
+  {
+    name: "ChatContent",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://us1.prisma.sh/noh-hyeon-jin-54dd46/SiBun/dev`
+});
+exports.prisma = new exports.Prisma();
