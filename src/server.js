27fcd8a1d1df23991ffd2f8aabc469e6acd08303
Noh Hyeon Jin <hjforj@naver.com>
2019-10-25 22:09:25 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
-require("dotenv").config();
+import path from "path";
+import "./env";
+
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
+import passport from "passport";
+import utils from "./utils";    //utils.js파일 import
+import "./passport";    //passport.js파일 import
+
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({ schema });
