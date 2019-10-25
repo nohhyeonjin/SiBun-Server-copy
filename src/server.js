5f7ceda4f044662bcbcf313e8915f0a5823c20c7
Noh Hyeon Jin <hjforj@naver.com>
2019-10-25 23:12:39 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
-import path from "path";
 import "./env";
-
 import { GraphQLServer } from "graphql-yoga";
+import { prisma } from "../generated/prisma-client";
 import logger from "morgan";
 import schema from "./schema";
-import passport from "passport";
 import utils from "./utils";    //utils.js파일 import
 import "./passport";    //passport.js파일 import
+import { authenticateJwt } from "./passport";
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema });
+const server = new GraphQLServer({ 
+    schema, 
+    context: ({request})=>({request})   //context는 resolver 사이에서 정보 공유 시 사용함
+});  
 server.express.use(logger("dev"));
+server.express.use(authenticateJwt);
 server.start({port: PORT}, () => console.log(`Server running on http://localhost:${PORT}`) );
