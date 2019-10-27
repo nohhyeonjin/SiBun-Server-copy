abeaf00c8c698bfcba66d16924f99e446c7fd662
Noh Hyeon Jin <hjforj@naver.com>
2019-10-28 03:47:22 +0900

    create updateLtitude

---
+++ src/api/Chat/updateLtitude/updateLtitude.js
@@ -0,0 +1,37 @@
+import { isAuthenticated } from "../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client";
+import Axios from "axios";
+
+export default{
+    Mutation:{
+        updateLtitude:async(_,args,{request})=>{
+            const { chatRoomId, address } = args;
+            var latitude = "latitude test";
+            var longtitude = "longtitude test";
+
+            //const address = '22 Main st Boston MA';
+            await Axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+                params:{
+                    address : address,
+                    key:'AIzaSyAu5_gGqUTAXcQzVQduOmTnNjpAn5rrG8E'
+                }
+            })
+            .then(function(response){
+                latitude = String(response.data.results[0].geometry.location.lat);
+                longtitude = String(response.data.results[0].geometry.location.lng);    
+            })
+            .catch(function(error){
+                console.log(error);
+            })           
+
+            const chatRoom = await prisma.updateChatRoom({
+                data:{
+                    latitude: latitude,
+                    longtitude: longtitude
+                },
+                where : { id : chatRoomId }
+            });
+            return chatRoom;
+        }
+    }
+}
