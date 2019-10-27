dec6f206441101dde93c8c6f6525147c45d3dd90
rosf73 <47631768+rosf73@users.noreply.github.com>
2019-10-28 02:58:37 +0900

    room의 채팅 목록을 확인하는 파트를 mutation으로 변경하였습니다

--- src/api/Chat/chatContent/chatContent.js
+++ src/api/Chat/chatContent/chatContent.js
@@ -1,17 +1,17 @@
 import { prisma } from "../../../../generated/prisma-client";
 
 //전체 chat 친구
-  export default{
-    Query: {
-        chatContents: async(_,args) => {
-          const chatRoomId = args;
-          const contents = await prisma.chatContents({
-          where : {chatRoom : chatRoomId},  
-          })
-            
-          return contents;
-        }
-    }
+export default{
+  Mutation: {
+      chatContents: async(_,args) => {
+        const { roomId } = args;
+        const contents = await prisma.chatContents({
+          where : { chatRoom: { id: roomId } }
+        })
+          
+        return contents;
+      }
+  }
 }
 
 
