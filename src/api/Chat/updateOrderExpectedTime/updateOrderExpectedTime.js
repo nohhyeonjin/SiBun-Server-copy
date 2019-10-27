ceeafbdf3bd2e455a7586d816ada211accec7e30
Noh Hyeon Jin <hjforj@naver.com>
2019-10-27 16:51:13 +0900

    Update updateOrderExpectedTime.js

--- src/api/Chat/updateOrderExpectedTime/updateOrderExpectedTime.js
+++ src/api/Chat/updateOrderExpectedTime/updateOrderExpectedTime.js
@@ -1,31 +1,15 @@
 import { prisma } from "../../../../generated/prisma-client";
-import {generateToken} from "../../../utils";
 
-
-// export default{
-//     Mutation:{
-//         updateOrderExpectTime:async(_,args,{request})=>{
-//             const {chatRoomID, changeExpectTime}=args;  
-//             const chatRoom = await prisma.chatRoom({chatRoomID});   //prisma.user�Լ��� �й��� ���ڷ� �Է��ؼ� ����� ��������
-//             if(user.pwd===pwd){ //���� user.pwd�� pwd�� ���ٸ� jwt ��ū ����
-//                 //JWT ��ū ����
-//                 const token = generateToken(user.id);   //jwt�� id�� ��ȣȭ�ؼ� ��ū�� �������
-//                 return token;  
-//             } else{
-//                 throw Error("Wrong number or pwd")
-//             }
-//         }
-//     }
-// }
-
-// export default{
-//     Mutation:{
-//         async updateOrderExpectTime(root, { _id, input }) {
-//             return await chatRoom.findOneAndUpdate(
-//                 { _id }, 
-//                 input, 
-//                 { new: true }
-//              );
-//         }
-//     }
-// }
+export default{
+    Mutation: {
+        updateOrderExpectedTime:async(_,args,{request})=>{
+            const { roomId, time } = args;
+            
+            const chatRoom = await prisma.updateChatRoom({
+                data : { orderExpectedTime : time },
+                where : { id : roomId }
+            })
+            return chatRoom;
+        }
+    }
+}
