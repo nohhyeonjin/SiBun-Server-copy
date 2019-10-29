1e5ca24753b9a3e5953705bf25c44cd35fc233ee
rosf73 <47631768+rosf73@users.noreply.github.com>
2019-10-29 12:55:13 +0900

    본인 확인 resolver 추가

--- src/api/User/compute.js
+++ src/api/User/compute.js
@@ -3,6 +3,12 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   User: {
     bossChatList: ({ id }) => prisma.user({ id }).bossChatList(),
-    chatList: ({ id }) => prisma.user({ id }).chatList()
+    chatList: ({ id }) => prisma.user({ id }).chatList(),
+    isMe: (parent, _, { request }) => {
+      const { id } = parent;
+      const { user } = request;
+      
+      return id === user.id;
+    }
   }
 }
