12061ee7a1a509aee9fbb3387a3951061764f42e
sungHee96 <nsh722@naver.com>
2019-11-11 20:54:01 +0900

    Vote랑 연관되어 있는 userVote가 한번에 delete 되도록 변경

--- src/api/Vote/removeVote/removeVote.js
+++ src/api/Vote/removeVote/removeVote.js
@@ -1,9 +1,17 @@
 import { prisma } from "../../../../generated/prisma-client";
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from "constants";
 
 export default{
     Mutation:{
         removeVote:async(_,args,{request})=>{
             const { voteId } = args;
+
+            const userVoteList = await prisma.userVotes({where : {vote : {id : voteId}}});
+            for(var i = 0; i < userVoteList.length; i++){
+                await prisma.deleteUserVote({
+                    id: userVoteList[i].id
+                });
+            }
             const vote = await prisma.deleteVote({
                 id : voteId
             });
