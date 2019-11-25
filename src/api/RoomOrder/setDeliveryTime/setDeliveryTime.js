17f1ef37baa4c59ffd61bba632cb6da8c6a28eac
rosf73 <47631768+rosf73@users.noreply.github.com>
2019-11-25 23:03:49 +0900

    배달 알림 메시지 전송

--- src/api/RoomOrder/setDeliveryTime/setDeliveryTime.js
+++ src/api/RoomOrder/setDeliveryTime/setDeliveryTime.js
@@ -1,13 +1,36 @@
 import { prisma } from "../../../../generated/prisma-client";
 
+const admin = require('firebase-admin');
+
 export default{
     Mutation:{
-        setDeliveryTime:async(_,args,{request})=>{
-            const { roomOrderId, deliveryTime } = args;
+        setDeliveryTime: async (_, args) => {
+            const { roomId, deliveryTime } = args;
            
+            const roomOrderId = await prisma.updateChatRoom({
+                data : { state: 4 },
+                where : { id : roomId }
+            }).roomOrder().id();
             const roomOrder = await prisma.updateRoomOrder({
                 data: { deliveryTime },
-                where:  {id : roomOrderId}
+                where:  { id : roomOrderId }
+            });
+            const storeName = await prisma.chatRoom({ id: roomId }).store().name();
+
+            const message = [{
+              notification: {
+                title: `${storeName}`,
+                body: `배달이 ${deliveryTime}분 안에 도착합니다`
+              },
+              topic: roomId
+            }];
+      
+            admin.messaging().sendAll(message)
+            .then((response) => {
+              console.log('Successfully sent message:', response);
+            })
+            .catch((error) => {
+              console.log('Error sending message:', error);
             });
         
             return roomOrder;
