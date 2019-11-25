a351594a5dbf6c85da12568717eecfe3121bd13f
rosf73 <47631768+rosf73@users.noreply.github.com>
2019-11-25 23:18:55 +0900

    통합된 ChatRoomState를 다룰 수 있도록 함

--- src/api/Chat/updateChatRoomState/updateChatRoomState.js
+++ src/api/Chat/updateChatRoomState/updateChatRoomState.js
@@ -1,16 +1,38 @@
-import { isAuthenticated } from "../../../middlewares";
 import { prisma } from "../../../../generated/prisma-client";
+
+const admin = require('firebase-admin');
 
 export default{
     Mutation: {
-        updateChatRoomState:async(_,args,{request})=>{
-            isAuthenticated(request);
-            const {roomId} = args;
+        updateChatRoomState: async (_, args) => {
+            const { roomId, state } = args;
+            // state가 1이면 기본, 2이면 승낙 대기, 3이면 주문 승낙
             
             const chatRoom = await prisma.updateChatRoom({
-                data : { state : true },
+                data : { state },
                 where : { id : roomId }
+            });
+            const storeName = await prisma.chatRoom({ id: roomId }).store().name();
+            
+            var msg = "";
+            if(state === 2) msg = "주문을 요청하였습니다";
+            if(state === 3) msg = "주문이 승낙되었습니다";
+            const message = [{
+              notification: {
+                title: `${storeName}`,
+                body: msg
+              },
+              topic: roomId
+            }];
+      
+            admin.messaging().sendAll(message)
+            .then((response) => {
+              console.log('Successfully sent message:', response);
             })
+            .catch((error) => {
+              console.log('Error sending message:', error);
+            });
+
             return chatRoom;
         }
     }
