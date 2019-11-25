ac49c4ba6a4a94e0ce92809be75e00dc4efe828e
rosf73 <47631768+rosf73@users.noreply.github.com>
2019-11-25 10:02:53 +0900

    새로운 메시지가 오면 알림 메시지 송출

--- src/api/Chat/chat/chat.js
+++ src/api/Chat/chat/chat.js
@@ -1,26 +1,41 @@
 import { prisma } from "../../../../generated/prisma-client";
 import { isAuthenticated } from "../../../middlewares";
+
+const admin = require('firebase-admin');
 // import { GraphQLServer, PubSub } from "graphql-yoga";
 
 // const pubSub = new PubSub();
 
-  export default{
-    Mutation:{
-        sendChat:async(_,args ,{ request }) =>{
-          isAuthenticated(request);
-          const { roomId , content } = args;
-          const { user } = request;
+export default{
+  Mutation:{
+    sendChat:async(_,args ,{ request }) =>{
+      isAuthenticated(request);
+      const { roomId , content } = args;
+      const { user } = request;
 
-          const chatContent = await prisma.createChatContent({
-            user: {connect : { id : user.id}},
-            chatRoom: {connect : { id : roomId}},
-            content: content
-          })
+      const chatContent = await prisma.createChatContent({
+        user: {connect : { id : user.id}},
+        chatRoom: {connect : { id : roomId}},
+        content: content
+      });
 
-          return "sendChat success"
-        }
-        
-        
-      } //chat
-  }
+      const message = [{
+        notification: {
+          title: '새로운 채팅 알림',
+          body: `${content}`
+        },
+        topic: roomId
+      }];
 
+      admin.messaging().sendAll(message)
+      .then((response) => {
+        console.log('Successfully sent message:', response);
+      })
+      .catch((error) => {
+        console.log('Error sending message:', error);
+      });
+
+      return "sendChat success"
+    }
+  } //chat
+}
