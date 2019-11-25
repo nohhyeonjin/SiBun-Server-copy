099a4661e475d4fd749da670282307e31b8b7f89
rosf73 <47631768+rosf73@users.noreply.github.com>
2019-11-25 23:05:11 +0900

    채팅방 변경 구독이 아닌 가게 주문 추가 구독으로 변경

--- src/api/Store/subscriptStoreOrder/subscriptStoreOrder.js
+++ src/api/Store/subscriptStoreOrder/subscriptStoreOrder.js
@@ -1,14 +1,13 @@
 import { prisma } from "../../../../generated/prisma-client";
 
-
-export default{
-    Subscription: {
-        subscriptStoreOrder: {
-          subscribe: (_, { storeId }) => prisma.$subscribe.chatRoom({
-            mutation_in: ['UPDATED'],
-            node: { store: {id: storeId}, state: true }
-          }).node(),
-          resolve: payload => payload
-        }
-      }
+export default {
+  Subscription: {
+    subscriptStoreOrder: {
+      subscribe: (_, { storeId }) => prisma.$subscribe.storeOrder({
+        mutation_in: ['CREATED'],
+        node: { store: { id: storeId } }
+      }).node(),
+      resolve: payload => payload
     }
+  }
+}
