989be0ca986ef2067d3b065e8493aae592e95ac8
sungHee96 <nsh722@naver.com>
2019-11-25 20:02:21 +0900

    getStoreOrderList추가

---
+++ src/api/Store/getStoreOrderList/getStoreOrderList.js
@@ -0,0 +1,14 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    getStoreOrderList: async (_, args) => {
+      const { storeId } = args;
+      const storeOrders = await prisma.storeOrders({
+        where : { store: { id: storeId } }
+      });
+
+      return storeOrders;
+    }
+  }
+}
