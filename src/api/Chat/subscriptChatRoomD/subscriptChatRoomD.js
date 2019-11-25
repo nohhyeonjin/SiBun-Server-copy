197f91ee0d961cae45a038d69fc50fda30762048
rosf73 <47631768+rosf73@users.noreply.github.com>
2019-11-25 13:08:41 +0900

    Update subscriptChatRoomD.js

--- src/api/Chat/subscriptChatRoomD/subscriptChatRoomD.js
+++ src/api/Chat/subscriptChatRoomD/subscriptChatRoomD.js
@@ -4,7 +4,7 @@ export default{
 Subscription: {
     subscriptChatRoomD: {
       subscribe: () => prisma.$subscribe.chatRoom({
-        mutation_in: ['DELETED']
+        mutation_in: ['DELETED', 'UPDATED']
       }).previousValues(),
       resolve: payload => payload
     }
